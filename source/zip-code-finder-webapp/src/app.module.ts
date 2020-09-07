4a45ad09a8942eb5774245baec3006aa45e371a3
Andre G Caires <andregcaires@outlook.com>
2020-09-07 17:39:16 -0300

    swagger added

--- source/zip-code-finder-webapp/src/app.module.ts
+++ source/zip-code-finder-webapp/src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ZipCodeController } from './zip-code/zip-code.controller';
 import { ZipCodeService } from './zip-code/zip-code.service';
 import { ZipCodeModule } from './zip-code/zip-code.module';
 
 @Module({
   imports: [ZipCodeModule],
-  controllers: [AppController, ZipCodeController],
-  providers: [AppService, ZipCodeService],
+  controllers: [ZipCodeController],
+  providers: [ZipCodeService],
 })
 export class AppModule {}
--- source/zip-code-finder-webapp/src/main.ts
+++ source/zip-code-finder-webapp/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('Zip Code Finder')
+    .setDescription('Finds address by its zip code')
+    .setVersion('1.0')
+    .addTag('zipcode')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
